6ccba579ec6239de8277a533ac547a26254e6c21
Patrick Gund <patrick.gund@gmail.com>
2018-03-27 10:37:00 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const compression = require('compression');
+const path = require('path');
+const PORT = process.env.PORT || 8080;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use(compression());
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+});
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
+
