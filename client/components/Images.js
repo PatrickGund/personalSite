01fd1db6f3f06b50b944e2a83d36b2264400a4df
Patrick Gund <patrick.gund@gmail.com>
2018-03-28 17:01:06 -0500

    minor change

--- client/components/Images.js
+++ client/components/Images.js
@@ -6,7 +6,7 @@ const Images = props => (
   <section id="carousel">
     <div id="carousel-text">
       <h1 id="imageTitle" >Patrick Gund</h1>
-      <h2>Full-Stack Software Engineer</h2>
+      <h2 id="imageText">Full-Stack Software Engineer</h2>
     </div>
     <img className="carousel-image" src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
   </section>
