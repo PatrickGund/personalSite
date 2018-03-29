a6e7a3cd7f8703e0c1043568816cb9660f76ac2b
Patrick Gund <patrick.gund@gmail.com>
2018-03-29 16:10:00 -0500

    remove useless props

--- client/components/Images.js
+++ client/components/Images.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 
-const Images = props => (
+const Images = () => (
   <section id="carousel">
     <div id="carousel-text">
       <h1 id="imageTitle" >Patrick Gund</h1>
       <h2 id="imageText">Full-Stack Software Engineer</h2>
     </div>
-    <img className="carousel-image" src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+    <img className="carousel-image" alt="computerImg" src="https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
   </section>
 );
 
--- client/components/NavMenu.js
+++ client/components/NavMenu.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 
-const NavMenu = props => (
+const NavMenu = () => (
   <header>
     <nav>
       <div className="logo">
